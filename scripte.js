2482ce41df3e9a8ddbfcda1b88c69276f8090128
HammadM4hmood <hammadmah9@users.noreply.github.com>
2023-09-15 10:06:51 -0600

    Create a Dummy Website
    Create a dummy website with smooth scroll, and gsap on the header

---
+++ scripte.js
@@ -0,0 +1,26 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 1.5,
+  });
+}
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      "#minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+  });
+}
+
+circleMouseFollower();
+firstPageAnim();
